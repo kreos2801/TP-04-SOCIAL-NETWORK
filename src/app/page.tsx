cf8eada24f743492e64e032840f667be850e0b64
gaouditz <gaouditz@et.esiea.fr>
2025-03-09 19:03:55 +0100

    error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
+"use client";
+
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <SignedOut>
-          <SignInButton mode='modal'>
-            <button className="bg-red-500">Sign in</button>
-          </SignInButton>
-        </SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-red-500">Sign in</button>
+        </SignInButton>
+      </SignedOut>
 
-        <SignedIn>
-          <UserButton />
+      <SignedIn>
+        <UserButton />
       </SignedIn>
     </div>
   );
