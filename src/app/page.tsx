26bf257c54c65b6bbc2d1c7da36f521c51ac820b
gaouditz <gaouditz@et.esiea.fr>
2025-03-10 11:11:53 +0100

    dark mode add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="bg-red-500">Sign in</button>
+          <Button>
+            Sign in
+          </Button>
         </SignInButton>
       </SignedOut>
 
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <ModeToggle/>
+      <Button variant={"secondary"}>click me</Button>
     </div>
   );
 }
