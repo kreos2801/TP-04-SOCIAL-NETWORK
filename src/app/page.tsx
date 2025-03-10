29baa4133de348a78174948dee582a920cc019e8
gaouditz <gaouditz@et.esiea.fr>
2025-03-10 11:51:35 +0100

    add nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 "use client";
 
-import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>
-            Sign in
-          </Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle/>
-      <Button variant={"secondary"}>click me</Button>
+      <h1>home page content</h1>
     </div>
   );
 }
