bb5a61221f3ddec8f4decae050cf9c6f31c0a0a3
gaouditz <gaouditz@et.esiea.fr>
2025-03-12 23:21:55 +0100

    Auth & DB Sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-"use client";
 
-import { Button } from "@/components/ui/button";
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+export default async function Home() {
 
-export default function Home() {
   return (
     <div className="m-4">
       <h1>home page content</h1>
