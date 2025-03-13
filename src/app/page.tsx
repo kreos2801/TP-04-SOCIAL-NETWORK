67e5ed66e11230f2bf98f3b2613e0db3b493dfe5
gaouditz <gaouditz@et.esiea.fr>
2025-03-13 11:26:52 +0100

    Like & Comment & Delete Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 // Assurez-vous que ce fichier est placé au bon endroit: app/(main)/page.tsx ou app/page.tsx
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const user = await currentUser();
-
+  const post = await getPosts();
+  const dbUserId = await getDbUserId();
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
+        <div className="space-y-6">
+          {post.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
         {!user && (
           <div className="rounded-lg border p-4">
             <h2 className="text-xl font-semibold mb-2">Welcome to Socially</h2>
