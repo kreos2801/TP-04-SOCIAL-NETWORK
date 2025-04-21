44d721529aa03b2ba6719359c0e5886a1dfd6eb0
gaouditz <gaouditz@et.esiea.fr>
2025-04-22 00:14:53 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    // GitHub Pages uses a subdirectory structure, so we need to set the base path
+    basePath: process.env.NODE_ENV === 'production' ? '/TP-04-SOCIAL-NETWORK' : '',
+    // To handle images properly in GitHub Pages
+    images: {
+      unoptimized: true,
+    },
+    // Disable server actions since we're using static export
+    experimental: {
+      serverActions: false,
+    }
+  };
+  
+  export default nextConfig;
